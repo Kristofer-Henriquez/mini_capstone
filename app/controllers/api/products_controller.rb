b29d3faf271d0470b4e1453cf6e05d1ec9e8878b
Kristofer <kristoferhenriquez@gmail.com>
2020-07-02 21:06:36 -0500

    deleted all routes, created index and show routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,47 +1,13 @@
 class Api::ProductsController < ApplicationController
 
-  def product_list
+  def index
     @products = Product.all
-    render "product_name.json.jb"
-  end
-  def product_cookie
-    @product_first = Product.first
-    render "product_first.json.jb"
-  end
-  def product_cake
-    @product_second = Product.second
-    render "product_second.json.jb"
-  end
-  def product_banana
-    @product_third = Product.third
-    render "product_third.json.jb"
+    render "index.json.jb"
   end
 
-  def any_product
-    product = params["product"]
-    @product = Product.find_by(id: product)
-    render "any_product.json.jb"
+  def show
+    @product = Product.find_by(id: params[:id])
+    render "show.json.jb"
   end
 
-  def guess_game
-    guess_answer = params[:number].to_i
-    if guess_answer > 32
-      @message = "you guessed too high"
-    elsif guess_answer < 32
-      @message = "you guessed too low"
-    else
-      @message = "That's correct!"
-    end
-    render "guess_game.json.jb"
-  end
-
-  def user_check
-    if params[:username] == "hugh" && params[:password] == "swordfish"
-      @message = "valid credentials"
-    else
-      @message = "invalid credentials"
-    end
-    render "check.json.jb"
-  end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,24 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
 
   namespace :api do
-    get "/Store" => "products#product_list"
-  
-  
-    get "/Store1" => "products#product_cookie"
-  
-  
-    get "/Store2" => "products#product_cake"
-  
-  
-    get "/Store3" => "products#product_banana"
-  
-  
-    get "/any_product" => "products#any_product"
-
-    get "/any_product/:product" => "products#any_product"
-
-    post "/guess_game" => "products#guess_game"
-
-    post "/username" => "products#user_check"
+    get "/products" => "products#index"
+    get "/products/:id" => "products#show"
   end
 end
