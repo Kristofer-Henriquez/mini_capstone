8ec3cae3f32035f564b53c9337510a02242d6d08
Kristofer <kristoferhenriquez@gmail.com>
2020-07-07 20:22:44 -0500

    Added Seed Gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.create!([
+  {name: "Banana", price: "0.0", image_url: "https://i.imgur.com/slf0cxq.jpg", description: "Not fun", stock: nil},
+  {name: "Apple", price: "2.0", image_url: "https://i.pinimg.com/236x/34/80/d9/3480d95898607afe6aee7b77bc611deb--apple-illustration-anime-tumblr.jpg", description: "This is a fruit", stock: nil},
+  {name: "Cake", price: "5.0", image_url: "https://i0.wp.com/cakesandcupcakesmumbai.com/wp-content/uploads/2012/12/cakes-cupcakes-mumbai-9.jpeg", description: "It's really good", stock: nil},
+  {name: "Milk", price: "1.0", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_usQe3X-CGTL8qe9rgAoZ2TycKzmxIcwOaA&usqp=CAU", description: "It's milk...", stock: nil},
+  {name: "Cookie", price: "1.0", image_url: "https://lh3.googleusercontent.com/proxy/S_yAFd6fnjsG22hAMvvlYXa0OpxM16yG833UOmAFC_ZnKO05yQJPLvfwXmoy98EWgUDCoOekx6whranQShkMuwSWSeY1WNk_A8AVd1GIajMmi9zN02XpZcdEDQq6WPQvGo8ZDAWHKBe2NqTjDeYf9SVgVL2bwyWExk8", description: "It's delicious", stock: nil}
+])
